fdace3a231c3513c5e05eeb0e0ca8a016148a38a
SuperGamingBros4 <60682866+SuperGamingBros4@users.noreply.github.com>
2021-04-22 01:41:43 -0700

    Update Game_List.lua

--- Game_List.lua
+++ Game_List.lua
@@ -1,4 +1,6 @@
-_G.GameIds = {5709572883, "Lifting_Legends_Simulator.lua",
-5817322596, "Speed_Legends_Simulator.lua",
-3652625463, "Lifting_Simulator_Auto_Farm.lua",
-6531005851, "Lifting_Titans.lua"}
+_G.GameIds = { --Structure {GameId, LuaFile}
+  5709572883, "Lifting_Legends_Simulator.lua",
+  5817322596, "Speed_Legends_Simulator.lua",
+  3652625463, "Lifting_Simulator.lua",
+  6531005851, "Lifting_Titans.lua"
+}
