473ce31b4b0987a0a42813a1650681c7dbf9af7a
SuperGamingBros4 <60682866+SuperGamingBros4@users.noreply.github.com>
2021-04-22 01:35:27 -0700

    Create Game_List.lua

---
+++ Game_List.lua
@@ -0,0 +1,4 @@
+_G.GameIds = {5709572883, "Lifting_Legends_Simulator.lua",
+5817322596, "Speed_Legends_Simulator.lua",
+3652625463, "Lifting_Simulator_Auto_Farm.lua",
+6531005851, "Lifting_Titans.lua"}
