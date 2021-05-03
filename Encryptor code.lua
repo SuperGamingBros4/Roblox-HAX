25af226a0888ef16cd8505230b04ea8d9cfef183
SuperGamingBros4 <60682866+SuperGamingBros4@users.noreply.github.com>
2021-05-02 21:27:12 -0700

    Create Encryptor code.lua

---
+++ Encryptor code.lua
@@ -0,0 +1,19 @@
+local input = _G.input
+local outputtable = {}
+function encrypt()
+    for i=1, input:len() do
+        table.insert(outputtable, string.byte(input, i))
+    end
+    local output = table.concat(outputtable, "\\")
+    _G.outputtable = outputtable
+    _G.output = output
+    setclipboard("\\"..output)
+    print("Encrypted, Encrypted script copied to your clipboard.")
+end
+function returndecrypt()
+    return(input)
+end
+function decrypt()
+    setclipboard(""..returndecrypt())
+	print("Decrypted, Decrypted script copied to your clipboard.")
+end
