76b9514e0f218cb09533222702ac93005ad083ff
SuperGamingBros4 <60682866+SuperGamingBros4@users.noreply.github.com>
2021-05-02 22:23:37 -0700

    Update Encryptor code.lua

--- Scripts/Encryptor code.lua
+++ Scripts/Encryptor code.lua
@@ -11,7 +11,7 @@ function encrypt()
     print("Encrypted, Encrypted script copied to your clipboard.")
 end
 function returndecrypt()
-   return(string.char(table.concat(string.split(input, "\\"), ", ")))
+   return(input)
 end
 function decrypt()
     setclipboard(""..returndecrypt())
