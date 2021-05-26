d1355cc80339df5c58ee97001875a01531b87a27
SuperGamingBros4 <60682866+SuperGamingBros4@users.noreply.github.com>
2021-05-25 21:44:35 -0700

    Update OP_Ninja_Simulator.lua

--- Scripts/OP_Ninja_Simulator.lua
+++ Scripts/OP_Ninja_Simulator.lua
@@ -3,7 +3,7 @@ while true do wait(0.001)
 	local Tool = Player.Character:FindFirstChildOfClass("Tool")
 	if Tool ~= nil then
 		if Tool.Handle ~= nil then
-			if (Tool:FindFirstChild("SwordSlash")) then
+			if (Tool.Handle:FindFirstChild("SwordSlash")) or (Tool:FindFirstChild("SwordSlash")) then
 				Tool:Activate()
 			end
 		end
