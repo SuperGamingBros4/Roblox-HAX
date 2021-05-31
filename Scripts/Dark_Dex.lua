ac2cc6c04756e2e5a29f8b3baf95929df55c3e8a
SuperGamingBros4 <60682866+SuperGamingBros4@users.noreply.github.com>
2021-05-30 18:08:30 -0700

    Create Dark_Dex.lua

---
+++ Scripts/Dark_Dex.lua
@@ -0,0 +1,38 @@
+local Dex = game:GetObjects("rbxassetid://3567096419")[1]
+Dex.Parent = PARENT
+local function Load(Obj, Url)
+	local function GiveOwnGlobals(Func, Script)
+		local Fenv = {}
+		local RealFenv = {script = Script}
+		local FenvMt = {}
+		FenvMt.__index = function(a,b)
+			if RealFenv[b] == nil then
+				return getfenv()[b]
+			else
+				return RealFenv[b]
+			end
+		end
+		FenvMt.__newindex = function(a, b, c)
+			if RealFenv[b] == nil then
+				getfenv()[b] = c
+			else
+				RealFenv[b] = c
+			end
+		end
+		setmetatable(Fenv, FenvMt)
+		setfenv(Func, Fenv)
+		return Func
+	end
+local function LoadScripts(Script)
+		if Script.ClassName == "Script" or Script.ClassName == "LocalScript" then
+			spawn(function()
+				GiveOwnGlobals(loadstring(Script.Source, "=" .. Script:GetFullName()), Script)()
+			end)
+		end
+		for i,v in pairs(Script:GetChildren()) do
+			LoadScripts(v)
+		end
+	end
+	LoadScripts(Obj)
+end
+load(Dex)
