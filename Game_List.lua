5a4ecac52bc22060bbe877347229c45320cd4326
SuperGamingBros4 <60682866+SuperGamingBros4@users.noreply.github.com>
2021-06-24 23:52:48 -0700

    Update Game_List.lua

--- Game_List.lua
+++ Game_List.lua
@@ -9,6 +9,6 @@ getgenv().GameIds = { --Structure {GameId, LuaFile}
   6245143799, "Minecraft.lua",
   1283960312, "Cash_Grab.lua",
   5733777811, "LockPicking_Simulator.lua",
-  6872265039, "Bed_Wars.lua"
+  6872265039, "Bed_Wars.lua",
   6872274481, "Bed_Wars.lua"
 }
