19f6adaa80e71050fe9c2413bd1a36bb5c0743e3
SuperGamingBros4 <60682866+SuperGamingBros4@users.noreply.github.com>
2021-06-24 17:25:37 -0700

    Update Game_List.lua

--- Game_List.lua
+++ Game_List.lua
@@ -1,13 +1,13 @@
-return { --Structure {GameId, LuaFile}
-  4695094789 = "Summer_Event_Weight_Lifting_Simulator.lua",
-  5709572883 = "Lifting_Legends_Simulator.lua",
-  5817322596 = "Speed_Legends_Simulator.lua",
-  3652625463 = "Lifting_Simulator.lua",
-  6531005851 = "Lifting_Titans.lua",
-  6273865312 = "SuperNatural_Simulator.lua",
-  4225025295 = "OP_Ninja_Simulator.lua",
-  6245143799 = "Minecraft.lua",
-  1283960312 = "Cash_Grab.lua",
-  5733777811 = "LockPicking_Simulator.lua"
-  6872265039 = "Bed_Wars.lua"
+getgenv().GameIds = { --Structure {GameId, LuaFile}
+  4695094789, "Summer_Event_Weight_Lifting_Simulator.lua",
+  5709572883, "Lifting_Legends_Simulator.lua",
+  5817322596, "Speed_Legends_Simulator.lua",
+  3652625463, "Lifting_Simulator.lua",
+  6531005851, "Lifting_Titans.lua",
+  6273865312, "SuperNatural_Simulator.lua",
+  4225025295, "OP_Ninja_Simulator.lua",
+  6245143799, "Minecraft.lua",
+  1283960312, "Cash_Grab.lua",
+  5733777811, "LockPicking_Simulator.lua"
+  6872265039, "Bed_Wars.lua"
 }
