f723cd204fbcd0ddad529e3a113161fd021fa14e
SuperGamingBros4 <60682866+SuperGamingBros4@users.noreply.github.com>
2021-06-29 10:45:06 -0700

    improve game list

--- Game_List.lua
+++ Game_List.lua
@@ -1,14 +1,14 @@
 return { -- Structure: [GameId] = "gamescript",
-    [4695094789] = "Summer_Event_Weight_Lifting_Simulator.lua",
-    [5709572883] = "Lifting_Legends_Simulator.lua",
-    [5817322596] = "Speed_Legends_Simulator.lua",
-    [3652625463] = "Lifting_Simulator.lua",
-    [6531005851] = "Lifting_Titans.lua",
-    [6273865312] = "SuperNatural_Simulator.lua",
-    [4225025295] = "OP_Ninja_Simulator.lua",
-    [6245143799] = "Minecraft.lua",
-    [1283960312] = "Cash_Grab.lua",
-    [5733777811] = "LockPicking_Simulator.lua",
-    [6872265039] = "Bed_Wars.lua",
-    [6872274481] = "Bed_Wars.lua"
+    [4695094789] = { [Script] = "Summer_Event_Weight_Lifting_Simulator.lua", [Name] = "Sumer Even Weight Lifting Simulator"}
+    [5709572883] = { [Script] = "Lifting_Legends_Simulator.lua", [Name] = "Lifting Legends Simulator"}
+    [5817322596] = { [Script] = "Speed_Legends_Simulator.lua", [Name] = "Speed Legends Simulator"}
+    [3652625463] = { [Script] = "Lifting_Simulator.lua", [Name] = "Lifting Simulator"}
+    [6531005851] = { [Script] = "Lifting_Titans.lua", [Name] = "Lifting Titans"}
+    [6273865312] = { [Script] = "SuperNatural_Simulator.lua", [Name] = "SuperNatural Simulator"}
+    [4225025295] = { [Script] = "OP_Ninja_Simulator.lua", [Name] = "OP Ninja Simulator"}
+    [6245143799] = { [Script] = "Minecraft.lua", [Name] = "MinersCave"}
+    [1283960312] = { [Script] = "Cash_Grab.lua", [Name] = "Cash Grab Simulator"}
+    [5733777811] = { [Script] = "LockPicking_Simulator.lua", [Name] = "Lockpicking Simulator"}
+    [6872265039] = { [Script] = "Bed_Wars.lua", [Name] = "Bed Wars Lobby"}
+    [6872274481] = { [Script] = "Bed_Wars.lua" [Name] = "Bed Wars Game"}
 }
