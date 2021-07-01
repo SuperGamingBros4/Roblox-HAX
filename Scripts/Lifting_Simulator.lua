d4482a8224ce305fd2c048d91f76da51130bacef
SuperGamingBros4 <60682866+SuperGamingBros4@users.noreply.github.com>
2021-06-30 21:29:56 -0700

    Update Lifting_Simulator.lua

--- Scripts/Lifting_Simulator.lua
+++ Scripts/Lifting_Simulator.lua
@@ -3,8 +3,8 @@
 local Player = game:GetService("Players").LocalPlayer
 
 local gmt = getrawmetatable(game)
-local oldindex = gmt.__index
-local oldnamecall = gmt.__namecall
+local oldindex
+local oldnamecall
 
 if not AlreadyExecutedLiftingSimulatorThisIsSoLongSoNoOneWillUseThis then
 --Global Environment:
@@ -15,7 +15,7 @@ getgenv().Main = loadstring(game:HttpGet("https://raw.githubusercontent.com/Supe
 --Metatable Hooks:
 
 setreadonly(gmt, false)
-gmt.__namecall = newcclosure(function(self, ...)
+oldnamecall = hookfunction(gmt.__namecall, function(self, ...)
 	local method = getnamecallmethod()
 	if method == "Kick" then
 		wait(9e9)
@@ -23,7 +23,7 @@ gmt.__namecall = newcclosure(function(self, ...)
 	end
 	return oldnamecall(self, ...)
 end)
-gmt.__index = newcclosure(function(self, b)
+oldindex = hookfunction(gmt.__index, function(self, b)
 	if Main.Flags.SpeedToggle then
 		if b == "WalkSpeed" then
 			if Player.Character then
@@ -51,13 +51,22 @@ coroutine.resume(coroutine.create(function()
 	end
 end))
 
-game:GetService("RunService").RenderStepped:Connect(function()
+local c
+c = game:GetService("RunService").RenderStepped:Connect(function()
 	if Main.Flags.AutoLift and Player.Character then
 		for i,v in pairs(Player.Character:GetDescendants()) do
 			if v.Name == "Income_Tool_LocalScript" then
 				v.Parent:Activate()
 			end
 		end
+	end
+	if Main.Flags.ToggleKillPlayer then
+		pcall(function()
+			for i,v in pairs(Main.Flags.SelectedKillPlayer) do
+				game:GetService("Players")[v].Character.HumanoidRootPart.CFrame = game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame
+			end
+		end)
+		wait(0.001)
 	end
 end)
 
@@ -75,7 +84,6 @@ local function openshop()
 	firetouchinterest(Player.Character.HumanoidRootPart, game:GetService("Workspace").EffectStorage.Mark02.Detector, 0)
 	firetouchinterest(Player.Character.HumanoidRootPart, game:GetService("Workspace").EffectStorage.Mark02.Detector, 1)
 end
-
 local Window = Main:CreateWindow("Lifting Simulator - By SuperJumpMan63#3843")
 local MainTab = Window:AddTab("Main") do
 	MainTab:AddToggle({Name = "Auto Lift", Flag = "AutoLift"})
@@ -86,4 +94,7 @@ local MiscTab = Window:AddTab("Misc") do
 	MiscTab:AddButton({Name = "Open Shop", Callback = openshop})
 	MiscTab:AddToggle({Name = "Toggle Speed", Flag = "SpeedToggle"})
 	MiscTab:AddSlider({Name = "WalkSpeed", Flag = "WalkSpeed", Min = 16, Max = 5000})
+	MiscTab:AddToggle({Name = "Bring Players", Flag = "ToggleKillPlayer"})
+	MiscTab:AddText("Note: The bring is client-sided, but you can still attack people.")
+	MiscTab:AddPlayerDropDownList({Name = "Players", Flag = "SelectedKillPlayer"})
 end
