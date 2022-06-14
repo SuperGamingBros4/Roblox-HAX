6b6c9846cf02e7d72437c703656ce878f8a8f9b6
SuperGamingBros4 <60682866+SuperGamingBros4@users.noreply.github.com>
2022-06-13 20:27:31 -0700

    Update Mad_City_AutoFarm_ServerHop.lua

--- Scripts/Mad_City_AutoFarm_ServerHop.lua
+++ Scripts/Mad_City_AutoFarm_ServerHop.lua
@@ -195,9 +195,11 @@ function ServerHop()
     Teleport([[loadstring(game:HttpGet("https://raw.githubusercontent.com/SuperGamingBros4/Roblox-HAX/main/Scripts/Mad_City_AutoFarm_ServerHop.lua"))()]])
 	local x = {}
 	for _, v in ipairs(game:GetService("HttpService"):JSONDecode(game:HttpGetAsync("https://games.roblox.com/v1/games/" .. game.PlaceId .. "/servers/Public?sortOrder=Asc&limit=100")).data) do
-		if type(v) == "table" and v.maxPlayers > v.playing and v.id ~= game.JobId then
-			x[#x + 1] = v.id
-		end
+        pcall(function()
+            if type(v) == "table" and v.maxPlayers > v.playing and v.id ~= game.JobId then
+                x[#x + 1] = v.id
+            end
+        end)
 	end
 	if #x > 0 then
 		game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, x[math.random(1, #x)])
