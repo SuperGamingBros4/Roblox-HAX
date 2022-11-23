1607041a9190c97823c92b372c0f776f08ed6df6
SuperGamingBros4 <60682866+SuperGamingBros4@users.noreply.github.com>
2022-11-23 04:16:34 -0800

    Update Blox_Fruits_Auto_Observation.lua

--- Scripts/Blox_Fruits_Auto_Observation.lua
+++ Scripts/Blox_Fruits_Auto_Observation.lua
@@ -1,33 +1,9 @@
-repeat task.wait() until game:IsLoaded()
-repeat task.wait() until game:GetService("Players").LocalPlayer:FindFirstChild("PlayerGui")
-repeat task.wait() until game:GetService("Players").LocalPlayer.PlayerGui:FindFirstChild("Main")
-repeat task.wait() until game:GetService("Players").LocalPlayer.PlayerGui.Main:FindFirstChild("ChooseTeam")
-repeat task.wait() until game:GetService("Players").LocalPlayer.PlayerGui.Main.ChooseTeam:FindFirstChild("Container")
-task.wait(1)
-for i,v in pairs(getconnections(game:GetService("Players").LocalPlayer.PlayerGui.Main.ChooseTeam.Container.Pirates.Frame.ViewportFrame.TextButton.Activated)) do
-    v.Function()
-end
-
-local LocalPlayer = game:GetService("Players").LocalPlayer
-function GoTo(Position)
-    if LocalPlayer.Character.PrimaryPart:FindFirstChild("SSSS") then
-        LocalPlayer.Character.PrimaryPart:FindFirstChild("SSSS"):Destroy()
-    end
-    local SSSS = Instance.new("AlignPosition", LocalPlayer.Character.PrimaryPart)
-    SSSS.Name = "SSSS"
-    SSSS.MaxForce = math.huge
-    SSSS.MaxVelocity = speed
-    SSSS.Mode = Enum.PositionAlignmentMode.OneAttachment
-    SSSS.Position = Position
-    SSSS.Responsiveness = 200
-    SSSS.Attachment0 = LocalPlayer.Character.PrimaryPart:FindFirstChild("RootRigAttachment")
-    while EEEE and PlayerCheck() and (LocalPlayer.Character.PrimaryPart.Position - Position).Magnitude > 128 do
-        wait()
+wait()
     end
     LocalPlayer.Character.PrimaryPart.CFrame = CFrame.new(Position)
 end
 
-repeat wait() until LocalPlayer.Character and LocalPlayer.Character:FindFirstChild("CharacterReady")
+repeat task.wait() until LocalPlayer.Character and LocalPlayer.Character:FindFirstChild("CharacterReady")
 
 local ClosestEnemy = nil
 repeat
@@ -38,7 +14,7 @@ repeat
             ClosestEnemy = v
         end
     end
-    wait()
+    task.wait()
 until ClosestEnemy ~= nil
 
 GoTo(ClosestEnemy.PrimaryPart.Position + Vector3.new(1,1,1))
@@ -46,7 +22,7 @@ game:GetService("ReplicatedStorage").Remotes.CommE:FireServer("Ken", true)
 
 local MaxTime = 2000
 repeat
-    wait(0.01)
+    task.wait(0.01)
     MaxTime -= 1
 until LocalPlayer.Character.Humanoid.Health < LocalPlayer.Character.Humanoid.MaxHealth or MaxTime <= 0
 
