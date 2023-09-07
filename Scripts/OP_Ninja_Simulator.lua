4d0c69cdd5ad5249b15062d4999a7214eb0d83dd
SuperGamingBros4 <60682866+SuperGamingBros4@users.noreply.github.com>
2023-09-07 01:37:35 -0700

    Update OP_Ninja_Simulator.lua

--- Scripts/OP_Ninja_Simulator.lua
+++ Scripts/OP_Ninja_Simulator.lua
@@ -1,11 +1,48 @@
+getgenv().Farm = true
+
 local Player = game:GetService("Players").LocalPlayer
-while true do wait(0.001)
-	local Tool = Player.Character:FindFirstChildOfClass("Tool")
-	if Tool ~= nil then
-		for i, v in pairs(Tool:GetDescendants()) do
+local MainGui = Player.PlayerGui.MainGui
+local respawnButton = MainGui.SpawnF.SpawnImgBtn
+local buttonContainers = {
+    "Ascend",
+    "Class",
+    "Shuriken",
+    "Sword"
+}
+
+local mainLoop
+mainLoop = game:GetService("RunService").Stepped:Connect(function()
+    if respawnButton.Visible then
+        getconnections(respawnButton.MouseButton1Down)[1].Function()
+        return
+    elseif not MainGui.UpgradeF.Visible then
+        return
+    end
+
+    for i, button in (buttonContainers) do
+        getconnections(MainGui.UpgradeF[button .. "F"][button .. "ImgBtn"].MouseButton1Down)[1].Function()
+    end
+    
+	local sword = nil
+    for i,tool in pairs(Player.Backpack:GetChildren()) do
+        if tool:FindFirstChild("Handle") and tool.Handle:findFirstChild("SwordSlash") then
+            sword = tool
+        end
+    end
+    if sword == nil then
+        sword = Player.Character:FindFirstChildOfClass("Tool")
+    end
+        
+	if sword ~= nil then
+		for i, v in pairs(sword:GetDescendants()) do
 			if v.Name == "SwordSlash" then
-				Tool:Activate()
+                Player.Character.Humanoid:EquipTool(sword)
+				sword:Activate()
 			end
 		end
 	end
-end
+    
+    if not Farm then
+        mainLoop:Disconnect()
+    end
+end)
