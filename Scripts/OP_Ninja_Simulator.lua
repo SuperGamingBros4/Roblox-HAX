812e0c6b8b20ade656bd9372b0755a93d695ae17
SuperGamingBros4 <60682866+SuperGamingBros4@users.noreply.github.com>
2023-09-09 01:20:39 -0700

    Update OP_Ninja_Simulator.lua
    Much more efficient algorithm for picking out the sword.

--- Scripts/OP_Ninja_Simulator.lua
+++ Scripts/OP_Ninja_Simulator.lua
@@ -11,12 +11,32 @@ local buttonContainers = {
     "Sword"
 }
 
+--Anti-AFK
 game:GetService("Players").LocalPlayer.Idled:Connect(function()
     vu:CaptureController()
     vu:Button2Down(Vector2.new(0,0), cam.CFrame)
     wait(1)
     vu:Button2Up(Vector2.new(0,0), cam.CFrame)
 end)
+
+function getTool(textureId)
+    local tool = Player.Character:FindFirstChildOfClass("Tool")
+
+    if tool ~= nil and tool.TextureId == textureId then
+        --If the player is holding the tool.
+        return tool
+    end
+
+    --If the player is not holding the tool.
+    for i,object in pairs(Player.Backpack:GetChildren()) do
+        if object.TextureId == textureId then
+            return object
+        end
+    end
+
+    --If the tool is not found
+    return nil
+end
 
 local mainLoop
 mainLoop = game:GetService("RunService").Stepped:Connect(function()
@@ -28,27 +48,17 @@ mainLoop = game:GetService("RunService").Stepped:Connect(function()
         return
     end
 
+    --Loop through and press every upgade button.
     for i, button in (buttonContainers) do
         getconnections(MainGui.UpgradeF[button .. "F"][button .. "ImgBtn"].MouseButton1Down)[1].Function()
     end
     
-	local sword = nil
-    for i,tool in pairs(Player.Backpack:GetChildren()) do
-        if tool:FindFirstChild("Handle") and tool.Handle:findFirstChild("SwordSlash") then
-            sword = tool
-        end
-    end
-    if sword == nil then
-        sword = Player.Character:FindFirstChildOfClass("Tool")
-    end
+    --Get the sword using the getTool() function
+	local sword = getTool("http://www.roblox.com/asset/?id=1078764870")
         
 	if sword ~= nil then
-		for i, v in pairs(sword:GetDescendants()) do
-			if v.Name == "SwordSlash" then
-                Player.Character.Humanoid:EquipTool(sword)
-				sword:Activate()
-			end
-		end
+        Player.Character.Humanoid:EquipTool(sword)
+        sword:Activate()
 	end
     
     if not Farm then
